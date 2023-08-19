7df3f059c3181374e0b37017246af107120133ca
Sergey Alexashenko <s.aleksashenko@gmail.com>
2023-08-19 15:58:54 -0400

    it's over

--- scripts.js
+++ scripts.js
@@ -1,59 +1,78 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('techTree.json')
-        .then(response => response.json())
-        .then(data => {
-            const cy = cytoscape({
-                container: document.getElementById('cy'),
-                elements: createElements(data),
-                style: [
-                    {
-                        selector: 'node',
-                        style: {
-                            'label': 'data(label)',
-                            'text-valign': 'center',
-                            'text-halign': 'center',
-                            'background-color': '#888',
-                            'color': '#000',
-                            'width': 'label',
-                            'height': 'label',
-                            'padding': '10px'
-                        }
-                    },
-                    {
-                        selector: 'edge',
-                        style: {
-                            'curve-style': 'bezier',
-                            'target-arrow-shape': 'triangle',
-                            'line-color': '#888',
-                            'target-arrow-color': '#888'
-                        }
-                    }
-                ],
-                layout: {
-                    name: 'dagre'
-                }
-            });
+// Fetch the tech tree data from techTree.json
+fetch('techTree.json')
+    .then(response => response.json())
+    .then(data => {
+        const techData = data;
+        const rootNodes = techData.filter(item => item.dependencies.length === 0).map(item => '#' + item.id).join(', ');
 
-            cy.on('tap', 'node', function (evt) {
-                let node = evt.target;
-                cy.elements().style({ 'background-color': '#888', 'line-color': '#888', 'color': '#000' });
-                node.connectedEdges().style('line-color', '#f00');
-                node.style('background-color', '#f00');
-            });
+        // Initialize Cytoscape
+        const cy = cytoscape({
+            container: document.getElementById('cy'),
+            elements: createElements(techData),
+            style: [{
+                selector: 'node',
+                style: {
+                    'label': 'data(label)',
+                    'background-color': '#636363',
+                    'color': '#ffffff',
+                    'text-outline-color': '#636363',
+                    'text-outline-width': '2px'
+                }
+            }, {
+                selector: 'edge',
+                style: {
+                    'curve-style': 'bezier',
+                    'target-arrow-shape': 'triangle',
+                    'line-color': '#888888',
+                    'target-arrow-color': '#888888'
+                }
+            }, {
+                selector: '.highlighted',
+                style: {
+                    'background-color': '#FFD700',
+                    'line-color': '#FFD700',
+                    'target-arrow-color': '#FFD700',
+                    'color': '#000000',
+                    'text-outline-color': '#FFD700'
+                }
+            }],
+
+            layout: {
+                name: 'dagre',
+                rankDir: 'BT',  // Bottom-to-top direction
+                nodeDimensionsIncludeLabels: true,  // Ensures the labels are considered for layout spacing
+                spacingFactor: 1.5  // Adjust as needed for desired spacing
+            }
+
         });
-});
+
+        cy.on('tap', 'node', function (evt) {
+            let node = evt.target;
+            cy.elements().removeClass('highlighted');
+            node.connectedEdges().addClass('highlighted');
+            node.addClass('highlighted');
+        });
+
+    });
 
 function createElements(data) {
-    const a = 30.0;  // scaling factor for better spacing
+    const a = 3.0;
     const b = 1760001;
-    let nodes = [];
-    let edges = [];
-    for (let item of data) {
+    let elements = [];
+
+    data.forEach(item => {
         let y = a * Math.log10(item.year + b);
-        nodes.push({ data: { id: item.id, label: item.label }, position: { x: 0, y: -y } });  // inverted y for correct chronological order
-        for (let dep of item.dependencies) {
-            edges.push({ data: { source: dep, target: item.id } });
-        }
-    }
-    return nodes.concat(edges);
+        elements.push({
+            data: { id: item.id, label: item.label },
+            position: { y: y }
+        });
+
+        item.dependencies.forEach(dep => {
+            elements.push({
+                data: { id: item.id + dep, source: dep, target: item.id } // Reversed source and target
+            });
+        });
+    });
+
+    return elements;
 }
